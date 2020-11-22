ce102251ce94c96a60217abae194098f025a9968
Kemici Cagnard <kemici.cagnard@outlook.com>
2020-11-22 13:36:54 +0100

    first commit

---
+++ README.txt
@@ -0,0 +1,23 @@
+To launch the Project :
+
+# install dependencies
+
+$ npm init
+$ npm install mongodb express
+$ node index.js
+
+Once the app is launched, go to : http://localhost:3000/movie to check if that works.
+If you want to add/edit/delete data in the database, you'll need Postman.
+Once you want to post/put/delete a movie from the DB, you'll have to set the parameters to raw, and enter the data in JSON.
+Look at the screenshot here : https://puu.sh/GPN12/7017c9d7f0.png
+
+Developing process of the application.
+
+To begin, I started by reading all the description of the assignment, to understand what I had to do. I listed every feature that the app needed.
+After that, I logged in my Atlas/mongo DB account. I needed a Mongo Data Base to do the project properly.
+Once I was logged in, I created my database. I had to think about the kind of data the app would store. First, i thought of Streamers, adding their names, age, streamlink etc. but I thought that i needed different type of values like Booleans, Ints and Strings. I didn't find any way to use booleans for streamers data, so I chose to store movies.
+My movie database would store 3 type of data : Name, which is a String, Duration, which is a int, and Released, which is a boolean (if the film's released, the boolean is set to true, if not, it's set to false.)
+So I had everything I needed. I copied the link to my database and put in in the code, so my app would correctly connect to the Database.
+Once it was done, I developped the app using methods I had seen last year and this year.
+I added a app.get, to get every movie from the database, a app.post, that will put movies to the database. Once I had both of them, I checked that everything worked properly.
+With that done, I added app.put, to edit a movie, and app.delete, to delete a movie from the database. I, once more, checked that everything worked, and with that done, i was satisfied by my work.
---
+++ index.js
@@ -0,0 +1,77 @@
+// We require express and mongoDB, since we need to use them.
+const express = require('express')
+const app = express()
+const { MongoClient, ObjectID } = require("mongodb");
+
+// Atlas connection string, to get a connection to the database.                                                                                                                                        
+const url = "mongodb+srv://dbUser:19738246@cluster0.e9nfj.mongodb.net/movieData?retryWrites=true&w=majority"
+const client = MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+    // We check the connexion to the database, and if there's an error, we display it.
+    if(err) throw err;
+    const db = client.db('movieData')
+
+    //POST Request -  we get the body
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: false}))
+
+    // We get the list of movies
+    app.get("/movie", async (req, res) => {
+        try{
+            // we get and display the movie list
+            let movie = await db.collection('movie').find().toArray();
+            res.send(movie)
+        }
+        catch(e){ 
+            // If there's an error, we display it.
+            res.send({type: "error", message: "Error"})
+        }
+    })
+    
+    // Here, we post the movie in the database.
+    app.post("/movie", async ({body: {name, duration, released}}, res) => {
+        try{
+            // We check that the name is a string, duration a number, and released a boolean
+            if(typeof name === 'string' && typeof duration === 'number' && typeof released === "boolean"){
+                await db.collection("movie").insertOne({name, duration, released})
+                res.send({type: "success", message: "Movie added"})
+            } 
+            // If not, we put an Error
+            else {
+                res.sned({type: "error", message: "Error, name must be a string, duration a number and released, a boolean"})
+            }
+        }catch(e){
+            res.send({type: "error", message: "Error"})
+        }
+    })
+
+    // Request to delete a movie from the list
+    app.delete("/movie/:id", async ({params: {id}}, res) => {
+        try {
+            // if there's no error, we delete the movie.
+            await db.collection('movie').deleteOne({_id: ObjectID(id)})
+            res.send({type:"success", message: "Removed"})
+        }catch(e){
+            // if there's an error, we display it
+            res.send({type: "error", message: "Error"})
+        }
+        
+    })
+
+    // request to Update a movie that already exists
+    app.put('/movie/:id', async ({body:{name, duration, released}, params: {id}}, res) => {
+        try{
+            // We check that the name is a string, duration a number, and released a boolean for the updating
+            if(typeof name === 'string' && typeof duration === 'number' && typeof released === "boolean"){
+                await db.collection('movie').updateOne({_id: ObjectID(id)}, {$set: {name, duration, released}})
+                res.send({type: "success", message: "Updated"})
+            } 
+            // if there's an error, we display it
+            else {
+                res.send({type: "error", message: "Error"})
+            }
+        } catch(e){
+            res.send({type: "error", message: "Error"})
+        }
+    })
+    app.listen(3000)
+});
